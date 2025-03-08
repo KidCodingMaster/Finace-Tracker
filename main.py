81c2b16ca19eab262c1963c282736bbed1d6eb3f
KidCodingMaster <lenovotab12101@gmail.com>
2025-03-08 13:28:55 +0400

    Login done

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ from encryt import encrypt, decrypt
 
 def username_exists(username):
     with shelve.open("username.db") as db:
-        for i in db:
-            if i == username:
-                return True
-
-    return False
+        return username in db
 
 
 def signup(username, password):
     if username_exists(username):
         print("Username already found!")
-        return
+        return False
 
     with shelve.open("username.db") as db:
         db[username] = encrypt(password)
         print("Signed up!")
+        return True
+
+
+def login(username, password):
+    if not username_exists(username):
+        print("Username not found!")
+        return False
+
+    with shelve.open("username.db") as db:
+        for i in db.values():
+            if decrypt(i) == password:
+                print("Successfully logged in!")
+                return True
+            
+    print('Wrong password!')
+    return False
 
 
 def main():
@@ -29,6 +41,11 @@ def main():
         password = input("Password: ")
 
         signup(username, password)
+    elif signup_or_login.lower() == "login":
+        username = input("Username: ")
+        password = input("Password: ")
+
+        login(username, password)
     else:
         print("Please enter 'signup' or 'login'")
 
