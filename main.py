52b6f821498ed2f95d7fcb32a5aed004237cf4cc
KidCodingMaster <lenovotab12101@gmail.com>
2025-03-08 13:20:23 +0400

    Signup done

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 import shelve
+from encryt import encrypt, decrypt
+
+
+def username_exists(username):
+    with shelve.open("username.db") as db:
+        for i in db:
+            if i == username:
+                return True
+
+    return False
+
 
 def signup(username, password):
-    with shelve.open('')
+    if username_exists(username):
+        print("Username already found!")
+        return
+
+    with shelve.open("username.db") as db:
+        db[username] = encrypt(password)
+        print("Signed up!")
+
+
+def main():
+    signup_or_login = input("Do you want to signup or login? ")
+
+    if signup_or_login.lower() == "signup":
+        username = input("Username: ")
+        password = input("Password: ")
+
+        signup(username, password)
+    else:
+        print("Please enter 'signup' or 'login'")
+
+
+if __name__ == "__main__":
+    main()
